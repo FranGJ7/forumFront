909275785120c01711ce93f3c8e1d541ba3042b9
Fransérgio Gonçalves Júnior <fransergiogj7@gmail.com>
2023-03-28 23:56:13 -0300

    funcionalidades de front-end completa - falta responsividade

--- src/components/TopicRoom.tsx
+++ src/components/TopicRoom.tsx
@@ -1,12 +1,74 @@
-import { Dispatch, SetStateAction } from "react"
+import { Dispatch, FormEvent, SetStateAction, useContext, useEffect, useState } from "react"
 import { Topic } from "./Home"
+import io from "socket.io-client"
+import { User, UserContext } from "../App"
+import MessageBox from "./MessageBox"
 
 type TopicRoomProps = {
   topic: Topic
   setOpenTopic: Dispatch<SetStateAction<Topic | null>>
 }
 
+
+export type Message = {
+  _id: string
+  content: string
+  author?: User
+  createdAt: string
+}
+
+
+const socket = io("ws://localhost:3001");
+
+
+
 export default function TopicRoom({ topic, setOpenTopic }: TopicRoomProps) {
+
+  const { user } = useContext(UserContext)
+  const [messages, setMessages] = useState<Message[]>([])
+
+
+
+  useEffect(() => {
+    loadMessages()
+  }, [])
+
+  
+
+  useEffect(() => {
+    socket.emit("join_room", { name: user?.name, topicId: topic._id })
+
+    socket.on("new_message", (newMessage: Message) => {
+      setMessages((mostRecentState) => [...mostRecentState, newMessage])
+    })
+
+    return () => {
+      socket.emit("leave_room", { name: user?.name, topicId: topic._id })
+      socket.off("new_message")
+    }
+  }, [socket])
+
+  async function loadMessages() {
+    const data = await fetch(`http://localhost:3001/topics/${topic._id}`).then(res => res.json())
+    setMessages(data.messages)
+  }
+
+
+  async function handleSubmit(ev: FormEvent<HTMLFormElement>) {
+    ev.preventDefault()
+
+    const formData = new FormData(ev.currentTarget)
+    const content = formData.get("message");
+    ev.currentTarget.reset();
+    socket.emit("send_message", {
+      content,
+      author: user,
+      topicId: topic._id
+    })
+  }
+
+
+
   return (
     <main className="room">
       <header>
@@ -15,10 +77,12 @@ export default function TopicRoom({ topic, setOpenTopic }: TopicRoomProps) {
       </header>
 
       <section className="messages">
+      {messages.map(message => <MessageBox key={message._id} message={message} />)}
       </section>
+     
 
-      <form className="send-message-form inline-form">
-        <input type="text" name="message" id="message" />
+      <form className="send-message-form inline-form" onSubmit={handleSubmit}>
+        <input type="text" name="message" id="message" placeholder="Digite sua mensagem.." required />
         <button>Enviar</button>
       </form>
     </main>
